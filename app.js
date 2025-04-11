f7e848b58813ffe1506a60def278fdb96d77ab09
khalid alsayed <khalidadnanalsayed@Khalids-MacBook-Air.local>
2025-04-11 18:56:33 +0300

    Fix: Added DB name to MONGO_URI

--- app.js
+++ app.js
@@ -13,10 +13,7 @@ const csrf = require("csurf");
 const app = express();
 
 //mongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch((err) => {
   console.error("MongoDB Connection Error:", err.message);
