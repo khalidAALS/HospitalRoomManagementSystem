7de995645f71b59603f7429c41453486930e3a56
khalid alsayed <khalidadnanalsayed@Khalids-MacBook-Air.local>
2025-04-12 20:15:33 +0300

    Added lifecycle block to protect app settings from overwrite

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,18 +20,30 @@ resource "azurerm_app_service_plan" "main" {
   }
 }
 
-resource "azurerm_application_insights" "main" {
-  name                = "hospitalroommanagement-ai"
+resource "azurerm_linux_web_app" "main" {
+  name                = "hospitalroommanagement-iac"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  application_type    = "web"
+  service_plan_id     = azurerm_app_service_plan.main.id
+
+  site_config {}
 
   lifecycle {
     ignore_changes = [
-      workspace_id
+      app_settings["MONGO_URI"],
+      app_settings["APPINSIGHTS_INSTRUMENTATIONKEY"],
+      app_settings["APPLICATIONINSIGHTS_CONNECTION_STRING"]
     ]
   }
+
+  app_settings = {
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE"         = "false"
+    "APPINSIGHTS_INSTRUMENTATIONKEY"              = azurerm_application_insights.main.instrumentation_key
+    "APPLICATIONINSIGHTS_CONNECTION_STRING"       = azurerm_application_insights.main.connection_string
+    "MONGO_URI"                                    = var.mongo_uri
+  }
 }
+
 
 resource "azurerm_linux_web_app" "main" {
   name                = "hospitalroommanagement-iac"
