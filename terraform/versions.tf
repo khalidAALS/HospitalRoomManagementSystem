637b6187120596525d4d36737c840bba2d3be58a
khalid alsayed <khalidadnanalsayed@Khalids-MacBook-Air.local>
2025-04-13 01:24:28 +0300

    Added remote backend config to versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,9 +1,17 @@
 terraform {
   required_version = ">= 1.5"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "HospitalRG"
+    storage_account_name = "hospitaltfstate123"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
   }
 }
